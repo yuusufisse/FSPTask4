9513c065bc011abc1ee327dd07b1d0a5dd9066a6
Yusuf Farah <cyufa001@edu.xamk.fi>
2022-10-20 21:35:46 +0300

    Add project files.

---
+++ FSPTask4/Models/Doctor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FSPTask4.Models
+{
+    public class Doctor
+    {
+        public int Id { get; set; }
+        public string? DoctorName { get; set; }
+        public List<Patient>? Patients { get; set; }
+    }
+}
---
+++ FSPTask4/Models/Patient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FSPTask4.Models
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+        public string? PatientName { get; set; }
+        public DateTime? ReceptionTime { get; set; }
+        public List<Doctor>? Doctors { get; set; }
+    }
+}
