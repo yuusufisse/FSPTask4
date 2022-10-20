9cdea3a681bec265fd6d466c7eb4baab271ccb32
Yusuf Farah <cyufa001@edu.xamk.fi>
2022-10-20 21:52:30 +0300

    part 2 completed

---
+++ FSPTask4/Data/FSPTask4Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using FSPTask4.Models;
+
+namespace FSPTask4.Data
+{
+    public class FSPTask4Context : DbContext
+    {
+        public FSPTask4Context (DbContextOptions<FSPTask4Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<FSPTask4.Models.Doctor> Doctor { get; set; }
+
+        public DbSet<FSPTask4.Models.Patient> Patient { get; set; }
+    }
+}
